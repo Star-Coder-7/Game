7fff5543840816a079eee539fa30bfbe6b27cfe4
Shiven Singhal <19singsh@hitchinboys.co.uk>
2021-02-23 07:14:38 +0000

    initial commit

--- enemy.py
+++ enemy.py
@@ -4,44 +4,44 @@ from random import randint
 class Enemy:
 
     def __init__(self, name='Enemy', hitPoints=0, lives=1, maxHitPoints=0):
-        self.name = name
-        self.hitPoints = hitPoints
-        self.lives = lives
-        self.maxHitPoints = 0
-        self.alive = True
+        self._name = name
+        self._hitPoints = hitPoints
+        self._lives = lives
+        self._maxHitPoints = 0
+        self._alive = True
 
     def takeDamage(self, damage):
-        remainingPoints = self.hitPoints - damage
+        remainingPoints = self._hitPoints - damage
 
         if remainingPoints >= 0:
-            self.hitPoints = remainingPoints
-            print(f"{self.name} took {damage} points of damage and has {self.hitPoints} hit points left.")
+            self._hitPoints = remainingPoints
+            print(f"{self._name} took {damage} points of damage and has {self._hitPoints} hit points left.")
 
         else:
-            self.lives -= 1
-            self.hitPoints = 0
-            self.hitPoints = self.maxHitPoints
-            print(f"{self.name} took {damage} points of damage and has {self.hitPoints} hit points left.")
+            self._lives -= 1
+            self._hitPoints = 0
+            self._hitPoints = self._maxHitPoints
+            print(f"{self._name} took {damage} points of damage and has {self._hitPoints} hit points left.")
 
-            if self.lives <= 0:
-                print(f"{self.name} has successfully been defeated!!!")
+            if self._lives <= 0:
+                print(f"{self._name} has successfully been defeated!!!")
                 self.alive = False
 
             else:
-                print(f"{self.lives} lost a life!!!")
-                self.alive = True
+                print(f"{self._lives} lost a life!!!")
+                self._alive = True
 
     def regenerate(self, bonus):
         if randint(1, 2) == 1:
             bonus = randint(1, 3)
-            self.lives += bonus
+            self._lives += bonus
         else:
             bonus = randint(1, 3)
-            self.hitPoints += bonus
-            self.maxHitPoints += bonus
+            self._hitPoints += bonus
+            self._maxHitPoints += bonus
 
     def __str__(self):
-        return f"Name: {self.name}, Lives: {self.lives}, Hit points: {self.hitPoints}"
+        return f"Name: {self._name}, Lives: {self._lives}, Hit points: {self._hitPoints}"
 
 
 class Troll(Enemy):
@@ -51,14 +51,14 @@ class Troll(Enemy):
         super().__init__(name=name, lives=1, hitPoints=23)   # To call the super class Enemy
 
     def grunt(self):
-        print(f"Me {self.name}. {self.name} grunt at you!!!")
+        print(f"Me {self._name}. {self._name} grunt at you!!!")
 
     def stomp(self):
-        print(f"Me {self.name}. {self.name} stomp on the ground fiercely!!!")
+        print(f"Me {self._name}. {self._name} stomp on the ground fiercely!!!")
         
     def defend(self):
         if randint(1, 3) == 2:
-            print(f"{self.name} defended thyself!")
+            print(f"{self._name} defended thyself!")
 
     def takeDamage(self, damage):
         if not self.defend():
@@ -73,9 +73,9 @@ class TrollKing(Troll):
 
     def __init__(self, name):
         super().__init__(name=name)
-        self.lives = 5
-        self.hitPoints = 50
-        self.maxHitPoints = 50
+        self._lives = 5
+        self._hitPoints = 50
+        self._maxHitPoints = 50
 
     @staticmethod
     def grunt():
@@ -102,9 +102,9 @@ class TrollQueen(Troll):
 
     def __init__(self, name):
         super().__init__(name=name)
-        self.lives = 8
-        self.hitPoints = 45
-        self.maxHitPoints = 45
+        self._lives = 8
+        self._hitPoints = 45
+        self._maxHitPoints = 45
 
     @staticmethod
     def grunt():
@@ -133,14 +133,14 @@ class Vampire(Enemy):
         super().__init__(name=name, lives=3, hitPoints=38)
 
     def scary(self):
-        print(f"{self.name} is I. {self.name} will scare you!!!")
+        print(f"{self._name} is I. {self._name} will scare you!!!")
 
     def transform(self):
-        print(f"(Screech) Now me, {self.name} is a bat!!!")
+        print(f"(Screech) Now me, {self._name} is a bat!!!")
 
     def defend(self):
         if randint(1, 3) == 3:
-            print(f"{self.name} defended thyself!")
+            print(f"{self._name} defended thyself!")
 
     def takeDamage(self, damage):
         if not self.defend():
@@ -155,9 +155,9 @@ class VampireKing(Vampire):
 
     def __init__(self, name):
         super().__init__(name=name)
-        self.lives = 10
-        self.hitPoints = 75
-        self.maxHitPoints = 75
+        self._lives = 10
+        self._hitPoints = 75
+        self._maxHitPoints = 75
 
     @staticmethod
     def scary():
@@ -184,9 +184,9 @@ class VampireQueen(Vampire):
 
     def __init__(self, name):
         super().__init__(name=name)
-        self.lives = 8
-        self.hitPoints = 80
-        self.maxHitPoints = 80
+        self._lives = 8
+        self._hitPoints = 80
+        self._maxHitPoints = 80
 
     @staticmethod
     def scary():
